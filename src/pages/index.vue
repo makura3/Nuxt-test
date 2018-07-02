2a83e17e8391464d5bd76cac1580c6ea6cbb700f
makura3 <37650810+makura3@users.noreply.github.com>
2018-07-02 23:40:34 +0900

    update index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="wrap">
-    <div v-if="loading" class="loading">
-      <i class="fa fa-spinner fa-3x" aria-hidden="true"></i>
-    </div>
-    <transition-group v-else name="fade" tag="div" class="item-wrap">
-      <Item v-for="(item, index) in init" :key="index" :item="item" />
-    </transition-group>
-  </section>
+  <div>
+    <section class="wrap">
+      １ページ目
+    </section>
+    <section>
+      2ぺーじめ
+    </section>
+  </div>
 </template>
 
 <script>
@@ -14,61 +14,20 @@ import { mapState, mapGetters } from 'vuex'
 import Item from '~/components/panel/Item.vue'
 
 export default {
-  components: {
-    Item
-  },
-  computed: {
-    ...mapState({
-      loading: 'loading' //loadingというstateをloadingという名前で呼び出す
-    }),
-    ...mapGetters({
-      init: 'getItems' //getItemsというgetterをinitという名前で呼び出す
-    })
-  }
-  // computed: mapState({
-  //   // アロー関数は、コードをとても簡潔にできます！
-  //   // count: state => state.count,
-  //   // 文字列を渡すことは、`state => state.count` と同じです
-  //   // countAlias: 'count',
-  //   // `this` からローカルステートを参照するときは、通常の関数を使わなければいけません
-  //   // loading: 'loading',
-  //   init() {
-  //     return this.$store.getters.getItems
-  //   }
-  // })
+  // components: {
+  //   Item
+  // },
+  // computed: {
+  //   ...mapState({
+  //     loading: 'loading' //loadingというstateをloadingという名前で呼び出す
+  //   }),
+  //   ...mapGetters({
+  //     init: 'getItems' //getItemsというgetterをinitという名前で呼び出す
+  //   })
+  // }
 }
 </script>
 
 <style lang="scss" scoped>
-.loading {
-  align-items: center;
-  color: $gray;
-  display: flex;
-  flex-basis: auto;
-  height: 300px;
-  justify-content: center;
-}
 
-.item-wrap {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  margin: 35px 10px;
-}
-
-.item {
-  margin: 15px 5px;
-
-  @include min-mq(sm) {
-    margin: 15px 10px;
-  }
-}
-
-.fade-enter-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter {
-  opacity: 0;
-}
 </style>
