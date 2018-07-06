923c365c79462aacf0db9d02d94b4c4611bac5a7
makura3 <37650810+makura3@users.noreply.github.com>
2018-07-06 21:23:30 +0900

    add setting file.

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+// const StylelintPlugin = require('stylelint-webpack-plugin')
+
+// const title = 'Nuxtテスト'
+
+// const meta = [
+//   { charset: 'utf-8' },
+//   { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
+//   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+//   { hid: 'description', name: 'description', content: 'Nuxtテスト' },
+//   { hid: 'itempropName', itemprop: 'name', content: title },
+//   { hid: 'itempropDesc', itemprop: 'description', content: 'Nuxtテスト' }
+// ]
+
+module.exports = {
+  srcDir: 'src/',
+  // head: {
+  //   title,
+  //   meta,
+  //   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  //   bodyAttrs: {
+  //     class: 'body'
+  //   }
+  // },
+  build: {
+    //buildがあればここに
+  },
+  modules: [
+    //modulesがあればここに
+  ],
+  css: [
+    //cssがあればここに
+  ],
+  plugins: [
+    //pluginsがあればここに
+  ]
+}
